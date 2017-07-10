3f35f139f6d40bdae7c9e6a55d69915558d31b0d
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-07-10 13:15:59 -0400

    Etapa 1: se oculta el icono de flecha de clase js-back

--- js/script-index.js
+++ js/script-index.js
@@ -3,6 +3,9 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+	// ETAPA 1: Ocultar la flecha 
+	$(".js-back").hide();
+
 });
 
 
